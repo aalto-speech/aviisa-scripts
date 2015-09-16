05c2550849da2e2e70907541b5f4642a56d965c9
Peter Smit <peter.smit@aalto.fi>
2015-09-16 12:42:36 +0200

    Remove redundant code from 03-prune

--- 03_lmtraining/train_scripts/01-morfessor.sh
+++ 03_lmtraining/train_scripts/01-morfessor.sh
@@ -16,7 +16,7 @@ if [ -z $MORPH_TRAIN_OPTIONS ]; then
 
     cat $SOURCE_FILES | \
     sed 's/(.*$//' | \
-    morfessor-train $MORPH_TRAIN_OPTIONS -d ones -s $TRAIN_DIR/morfessor.bin --save-reduced -
+    morfessor-train $MORPH_TRAIN_OPTIONS -d ones -s $TRAIN_DIR/morfessor.bin -
     cat $SOURCE_FILES | \
     sed 's/(.*$//' | \
     morfessor-segment -l $TRAIN_DIR/morfessor.bin --output-format "{analysis} <w> " --output-newlines - > $TRAIN_DIR/lm_source_txt

07a8ff208c0a3e028692cd5c0dcd7c2af9b9c013
Peter Smit <peter.smit@aalto.fi>
2015-09-16 15:23:34 +0200

    Fix array syntax

--- 03_lmtraining/train_scripts/01-morfessor.sh
+++ 03_lmtraining/train_scripts/01-morfessor.sh
@@ -12,11 +12,11 @@ source "$1"
 mkdir -p $TRAIN_DIR
 
 SOURCE_FILES=(${SOURCE_FILES//:/ })
-echo $SOURCE_FILES
+echo ${SOURCE_FILES[@]}
 
 if [ ! -z ${MORPH_TRAIN_OPTIONS+x} ]; then
 
-    cat $SOURCE_FILES | \
+    cat ${SOURCE_FILES[@]} | \
     sed 's/(.*$//' | \
     morfessor-train $MORPH_TRAIN_OPTIONS -d ones -s $TRAIN_DIR/morfessor.bin -
     cat $SOURCE_FILES | \
@@ -24,7 +24,7 @@ if [ ! -z ${MORPH_TRAIN_OPTIONS+x} ]; then
     morfessor-segment -l $TRAIN_DIR/morfessor.bin --output-format "{analysis} <w> " --output-newlines - > $TRAIN_DIR/lm_source_txt
 
 else
-    cat $SOURCE_FILES | \
+    cat ${SOURCE_FILES[@]} | \
     sed 's/(.*$//' | \
     > $TRAIN_DIR/lm_source_txt
 

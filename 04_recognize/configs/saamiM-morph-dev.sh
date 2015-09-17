aa8f31ba1ceee1ab6bf4c1474165871e15b6ad90
Peter Smit <peter.smit@aalto.fi>
2015-09-17 17:34:16 +0200

    Male recognition configs

---
+++ 04_recognize/configs/saamiM-morph-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export TEST_NAME="saamiM-morph-dev"
+export TEST_DIR="$GROUP_DIR/p/sami/recog_tests/$TEST_NAME"
+
+
+export TEST_AM="$GROUP_DIR/p/sami/models/saami_male/hmm/saami_male_11.8.2015_22"
+
+export TEST_LM="$GROUP_DIR/p/sami/lmmodels/saamiM_2gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_3gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_4gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_5gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_6gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_7gram_morph":"$GROUP_DIR/p/sami/lmmodels/saamiM_8gram_morph"
+
+
+export TEST_TRN="$GROUP_DIR/p/sami/uit-sme-M/devel.trn"
+export TEST_WAVLIST="$GROUP_DIR/p/sami/uit-sme-M/devel.scp"
+
+export ONE_BYTE_ENCODING=ISO-8859-10
--- 04_recognize/configs/saamiM-word-dev.sh
+++ 04_recognize/configs/saamiM-word-dev.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 export TEST_NAME="saamiM-word-dev"
 export TEST_DIR="$GROUP_DIR/p/sami/recog_tests/$TEST_NAME"
 
 
 export TEST_AM="$GROUP_DIR/p/sami/models/saami_male/hmm/saami_male_11.8.2015_22"
 
-export TEST_LM="$GROUP_DIR/p/sami/lmmodels/saamiM_2gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_3gram_word"
+export TEST_LM="$GROUP_DIR/p/sami/lmmodels/saamiM_2gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_3gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_4gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_5gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_6gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_7gram_word":"$GROUP_DIR/p/sami/lmmodels/saamiM_8gram_word"
 
 
 export TEST_TRN="$GROUP_DIR/p/sami/uit-sme-M/devel.trn"

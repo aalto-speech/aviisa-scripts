c7355960600312c624cdfa919894684a9952753a
Peter Smit <peter@smitmail.eu>
2015-10-13 08:42:38 +0300

    fix vocab2lex

--- 02_amtraining/base_scripts/vocab2lex.py
+++ 02_amtraining/base_scripts/vocab2lex.py
@@ -29,10 +29,10 @@ def main(phone_map, abbreviations):
                 transcriptions.append([phone_map[c] for c in abbr if c in phone_map])
 
         for trans in transcriptions:
-            o.write("{} ({:.1f})\n".format(word, 1/len(transcriptions)).encode("utf-8"))
+            o.write("{} ({:.1f})  ".format(word, 1/len(transcriptions)).encode("utf-8"))
             rtrans = ["_"]+trans+["_"]
             for i in range(1, len(trans)+1):
-                o.write("{}-{}+{}".format(rtrans[i-1],rtrans[i],rtrans[i+1]).encode("iso-8859-15"))
+                o.write("{}-{}+{} ".format(rtrans[i-1],rtrans[i],rtrans[i+1]).encode("iso-8859-15"))
         o.write(b"\n")
 
 if __name__ == "__main__":

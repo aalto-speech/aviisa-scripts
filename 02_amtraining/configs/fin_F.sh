395f2f3b22164961b0a8d02e3a83ee95049bd2b1
Peter Smit <peter@smitmail.eu>
2015-10-14 10:37:32 +0300

    Add Finnish configs

---
+++ 02_amtraining/configs/fin_F.sh
@@ -0,0 +1,19 @@
+export TRAIN_NAME='fin_F'
+export TRAIN_DIR="$GROUP_DIR/p/sami/models/$TRAIN_NAME"
+export TRAIN_ALIGN_DIR="$TRAIN_DIR/align"
+
+export TRAIN_IM="$GROUP_DIR/p/sami/initial_models/speecon_mfcc_gain3500_occ225_mpe_19kmorphs_1gram_extviterbi_27.11.2007_4"
+
+export TRAIN_RECIPE="$TRAIN_DIR/recipe"
+export TRAIN_LEX="$TRAIN_DIR/train.lex"
+
+export TRAIN_TRN="$GROUP_DIR/p/sami/audio_data/fin-F/train.trn"
+export TRAIN_WAVLIST="$GROUP_DIR/p/sami/audio_data/fin-F/train.scp"
+
+export TRAIN_GAUSSIANS=30000
+export TRAIN_BATCHES=5
+#export TRAIN_CLUSTERS=5
+
+export TRAIN_SCRIPTDIR="$GROUP_DIR/Modules/src/git/AaltoASR/aku/scripts"
+export LANGDAT_DIR="$GROUP_DIR/p/sami/langdata/fin";
+export TRAIN_BINDIR="$GROUP_DIR/Modules/src/git/AaltoASR/build/aku"

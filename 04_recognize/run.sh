f0496102975561316490f7a5b05af3fbd5734d7b
Peter Smit <peter@smitmail.eu>
2015-10-19 11:34:55 +0300

    Make run scripts sbatchable

--- 04_recognize/run.sh
+++ 04_recognize/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#SBATCH --time=4:00:00 --mem-per-cpu=1000
+#SBATCH -p coin
+#SBATCH -n1 -N1
+
+
+module load AaltoASR sctk
+
 if [ -z "$1" ]; then
     echo "Provide a config file"
     exit

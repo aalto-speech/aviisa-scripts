4681e10e7970aa720f9ae65bff108bc4595b76d2
Peter Smit <peter@smitmail.eu>
2015-10-19 09:51:55 +0300

    Update paths

--- 03_lmtraining/configs/sme_F_vkn_10g_m.sh
+++ 03_lmtraining/configs/sme_F_vkn_10g_m.sh
@@ -1,7 +1,7 @@
 export TRAIN_NAME='sme_F_vkn_10g_m'
 export TRAIN_DIR="$GROUP_DIR/p/sami/lmmodels/$TRAIN_NAME"
 
-export SOURCE_FILES=$GROUP_DIR/p/sami/audio_files/sme_F/train.trn:$GROUP_DIR/p/sami/wikipedia.txt
+export SOURCE_FILES=$GROUP_DIR/p/sami/audio_data/sme_F/train.trn:$GROUP_DIR/p/sami/wikipedia.txt
 
 export NGRAM_ORDER=10
 

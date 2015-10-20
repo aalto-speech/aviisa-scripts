e8651c52752e0a4cf0b723e9168f6a2a1dae22bb
Peter Smit <peter@smitmail.eu>
2015-10-20 09:52:47 +0300

    Try wb discounting instead of kn

--- 03_lmtraining/train_scripts/02-ngram_count.sh
+++ 03_lmtraining/train_scripts/02-ngram_count.sh
@@ -21,19 +21,30 @@ varigram_kn $VARIKN_OPTIONS -3 -C -a -Z -U $TRAIN_DIR/vocab -n $NGRAM_ORDER -o $
 
 else
 
-INTERPOLATE=$(seq 1 $NGRAM_ORDER | sed "s/^/-interpolate/" | tr "\n" " ")
-KNDISCOUNT=$(seq 1 $NGRAM_ORDER | sed "s/^/-kndiscount/" | tr "\n" " ")
+for knd in $(seq $NGRAM_ORDER -1 1); do
 
 
-cat $TRAIN_DIR/lm_source_txt | \
-ngram-count \
-  -order $NGRAM_ORDER \
-  $INTERPOLATE \
-  -text - \
-  -lm $TRAIN_DIR/model.gz \
-  $KNDISCOUNT \
-  -write-vocab $TRAIN_DIR/vocab
+    INTERPOLATE=$(seq 1 $NGRAM_ORDER | sed "s/^/-interpolate/" | tr "\n" " ")
+    KNDISCOUNT=$(seq 1 $knd | sed "s/^/-kndiscount/" | tr "\n" " ")
+    WBDISCOUNT=""
+    if [ $knd != $NGRAM_ORDER ]; then
+        WBDISCOUNT=$(seq $((knd+1)) $NGRAM_ORDER | sed "s/^/-wbdiscount/" | tr "\n" " ")
+    fi
 
+    echo $KNDISCOUNT $WBDISCOUNT
+    cat $TRAIN_DIR/lm_source_txt | \
+    ngram-count \
+      -order $NGRAM_ORDER \
+      $INTERPOLATE \
+      -text - \
+      -lm $TRAIN_DIR/model.gz \
+      $KNDISCOUNT $WBDISCOUNT \
+      -write-vocab $TRAIN_DIR/vocab
+
+    if [ $? == 0 ]; then
+        break
+    fi
+done
 fi
   
    

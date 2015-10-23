d1746d5454ee8920af25df8fd33225fbc612d65a
Peter Smit <peter@smitmail.eu>
2015-10-23 08:13:44 +0300

    Update configs to use devel200

--- 04_recognize/configs/small1_wikipedia/gen_configs.py
+++ 04_recognize/configs/small1_wikipedia/gen_configs.py
@@ -21,7 +21,7 @@ for lang in ("sme", "est", "fin"):
                         print("export TEST_AM={}".format(am), file=f)
                         print("export TEST_LM=$GROUP_DIR/p/sami/lmmodels/small1_wikipedia/{}{}_s1w_{}_{}g_{}".format(lang,gender,tool,order,type), file=f)
 
-                        print("export TEST_TRN=$GROUP_DIR/p/sami/audio_data/{}_{}/devel.trn".format(lang, gender), file=f)
-                        print("export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/{}_{}/devel.scp".format(lang, gender), file=f)
+                        print("export TEST_TRN=$GROUP_DIR/p/sami/audio_data/{}_{}/devel200.trn".format(lang, gender), file=f)
+                        print("export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/{}_{}/devel200.scp".format(lang, gender), file=f)
 
                         print("export ONE_BYTE_ENCODING=ISO-8859-10", file=f)

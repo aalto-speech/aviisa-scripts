cc3de4bd3cb470f03fe2345dca26aece470d91a5
Peter Smit <peter@smitmail.eu>
2015-10-26 10:22:38 +0200

    update biglm configs

--- 03_lmtraining/configs/biglm/gen_configs.py
+++ 03_lmtraining/configs/biglm/gen_configs.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 
 for lang in ("sme", "est", "fin"):
-    for gender in ("M", "F"):
-        for order in [4,10,20]:
-            with open("{}{}_b_v_{}g_m.sh".format(lang,gender,order), 'w') as f:
-                print("export TRAIN_NAME='{}{}_b_v_{}g_m'".format(lang,gender,order), file=f)
-                print("export TRAIN_DIR=$GROUP_DIR/p/sami/lmmodels/biglm/$TRAIN_NAME", file=f)
-                print(file=f)
-                print("export SOURCE_FILES=$GROUP_DIR/p/sami/lmdata/{}/biglm.txt".format(lang), file=f)
-                print(file=f)
-                print("export NGRAM_ORDER={}".format(order), file=f)
-                print("export PRUNE_THRESHOLD=5e-9", file=f)
-                print(file=f)
-                print("export LA_NGRAM_ORDER={}".format(order//3), file=f)
-                print("export LA_PRUNE_THRESHOLD=5e-8", file=f)
+    for order in [4,10,20]:
+        with open("{}_b60k_v_{}g_m.sh".format(lang,order), 'w') as f:
+            print("export TRAIN_NAME='{}_b60k_v_{}g_m'".format(lang,order), file=f)
+            print("export TRAIN_DIR=$GROUP_DIR/p/sami/lmmodels/biglm/$TRAIN_NAME", file=f)
+            print(file=f)
+            print("export SOURCE_FILES=$GROUP_DIR/p/sami/lmdata/{}/biglm.txt".format(lang), file=f)
+            print(file=f)
+            print("export NGRAM_ORDER={}".format(order), file=f)
+            print("export PRUNE_THRESHOLD=5e-9", file=f)
+            print(file=f)
+            print("export LA_NGRAM_ORDER={}".format(order//3), file=f)
+            print("export LA_PRUNE_THRESHOLD=5e-8", file=f)
 
-                print("export MORPH_TRAIN_OPTIONS=\"\"", file=f)
+            print("export MORPH_TRAIN_OPTIONS=\" --num-morph-types=60000 \"", file=f)
 
-                print("export VARIKN_OPTIONS=\"\"", file=f)
-                count = 100000
-                print("export VARIKN_DEVSIZE={}".format(count), file=f)
+            print("export VARIKN_OPTIONS=\"-s\"", file=f)
+            count = 20000
+            print("export VARIKN_DEVSIZE={}".format(count), file=f)
--- 03_lmtraining/train_scripts/02-ngram_count.sh
+++ 03_lmtraining/train_scripts/02-ngram_count.sh
@@ -12,10 +12,14 @@ source "$1"
 
 if [ ! -z ${VARIKN_OPTIONS+x} ]; then
 
+if [ ! -f $TRAIN_DIR/varikn_train ]; then
 sort -R < $TRAIN_DIR/lm_source_txt > $TRAIN_DIR/lm_source_txt_rand
 head -n ${VARIKN_DEVSIZE} $TRAIN_DIR/lm_source_txt_rand > $TRAIN_DIR/varikn_dev
 tail -n +${VARIKN_DEVSIZE} $TRAIN_DIR/lm_source_txt_rand > $TRAIN_DIR/varikn_train
 
+rm $TRAIN_DIR/lm_source_txt_rand
+
+fi
 
 varigram_kn $VARIKN_OPTIONS -3 -C -a -Z -U $TRAIN_DIR/vocab -n $NGRAM_ORDER -o $TRAIN_DIR/varikn_dev $TRAIN_DIR/varikn_train "$TRAIN_DIR/${TRAIN_NAME}"
 

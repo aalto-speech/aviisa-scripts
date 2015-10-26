bb549980117046db5c3867c4eb086b2fed05357e
Peter Smit <peter@smitmail.eu>
2015-10-26 08:18:43 +0200

    orig configs

---
+++ 04_recognize/configs/orig/estM_o_v_10g_m.sh
@@ -0,0 +1,11 @@
+export TEST_NAME='estM_o_v_10g_m'
+export TEST_DIR=$GROUP_DIR/p/sami/recog_tests/orig/
+
+export TEST_LM_SCALES=30
+
+export TEST_AM=/triton/ics/project/puhe/p/sami/models/est_M/hmm/est_M_18.10.2015_22
+export TEST_LM=$GROUP_DIR/p/sami/lmmodels/orig/est_morph_40k
+export TEST_TRN=$GROUP_DIR/p/sami/audio_data/est_M/devel200.trn
+export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/est_M/devel200.scp
+
+export ONE_BYTE_ENCODING=ISO-8859-15
---
+++ 04_recognize/configs/orig/finM_o_v_6g_m.sh
@@ -0,0 +1,11 @@
+export TEST_NAME='finM_o_v_6g_m'
+export TEST_DIR=$GROUP_DIR/p/sami/recog_tests/orig/
+
+export TEST_LM_SCALES=30
+
+export TEST_AM=/triton/ics/project/puhe/p/sami/models/fin_M/hmm/fin_M_18.10.2015_22
+export TEST_LM=$GROUP_DIR/p/sami/lmmodels/orig/fin_morph_40k
+export TEST_TRN=$GROUP_DIR/p/sami/audio_data/fin_M/devel200.trn
+export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/fin_M/devel200.scp
+
+export ONE_BYTE_ENCODING=ISO-8859-15

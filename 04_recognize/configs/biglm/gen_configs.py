08d49f3fdb49fcfc58971df544637b6aeef43fe4
Peter Smit <peter@smitmail.eu>
2015-10-28 17:01:57 +0200

    Do it

--- 04_recognize/configs/biglm/gen_configs.py
+++ 04_recognize/configs/biglm/gen_configs.py
@@ -66,3 +66,25 @@ for lang in ("sme", "est", "fin"):
                 print("export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/{}_{}/devel200.scp".format(lang, gender), file=f)
 
                 print("export ONE_BYTE_ENCODING=ISO-8859-10", file=f)
+
+
+for lang in ("sme", "est", "fin"):
+    for gender in ("M", "F"):
+        for order in [4,10,20]:
+            with open("{}{}_s1b60kv_v_{}g_m.sh".format(lang, gender ,order), 'w') as f:
+                print("export TEST_NAME='{}{}_s1b60kv_v_{}g_m'".format(lang,gender,order), file=f)
+                print("export TEST_DIR=$GROUP_DIR/p/sami/recog_tests/biglm/", file=f)
+                print(file=f)
+                print("export TEST_LM_SCALES=30", file=f)
+                print(file=f)
+
+                possible_ams = list(glob.glob(os.environ["GROUP_DIR"]+"/p/sami/models/{}_{}_150m/hmm/*_22.ph".format(lang, gender)))
+                am = possible_ams[0][:-3]
+
+                print("export TEST_AM={}".format(am), file=f)
+                print("export TEST_LM=$GROUP_DIR/p/sami/lmmodels/biglm/{}_b60kv_v_{}g_m".format(lang,order), file=f)
+
+                print("export TEST_TRN=$GROUP_DIR/p/sami/audio_data/{}_{}/devel200.trn".format(lang, gender), file=f)
+                print("export TEST_WAVLIST=$GROUP_DIR/p/sami/audio_data/{}_{}/devel200.scp".format(lang, gender), file=f)
+
+                print("export ONE_BYTE_ENCODING=ISO-8859-10", file=f)

e5661cfb16333acdfb466d3429682056dbd76386
Peter Smit <peter@smitmail.eu>
2015-10-29 16:57:34 +0200

    wip

--- 03_lmtraining/configs/biglm/gen_configs.py
+++ 03_lmtraining/configs/biglm/gen_configs.py
@@ -40,3 +40,50 @@ for lang in ("sme", "est", "fin"):
             print("export VARIKN_OPTIONS=\"--nfirst=60000 --dscale=0.0025\"", file=f)
             count = 20000
             print("export VARIKN_DEVSIZE={}".format(count), file=f)
+
+
+lang = "sme"
+for order in range(2, 21):
+    with open("{}_b60kv_v_{}g_m.sh".format(lang,order), 'w') as f:
+        print("export TRAIN_NAME='{}_b60kv_v_{}g_m'".format(lang,order), file=f)
+        print("export TRAIN_DIR=$GROUP_DIR/p/sami/lmmodels/biglm/$TRAIN_NAME", file=f)
+        print(file=f)
+        print("export SOURCE_FILES=$GROUP_DIR/p/sami/lmdata/{}/biglm.txt".format(lang), file=f)
+        print(file=f)
+        print("export NGRAM_ORDER={}".format(order), file=f)
+        print("export PRUNE_THRESHOLD=5e-9", file=f)
+        print(file=f)
+        print("export LA_NGRAM_ORDER={}".format(order//3), file=f)
+        print("export LA_PRUNE_THRESHOLD=5e-8", file=f)
+
+        print("export MORPH_TRAIN_OPTIONS=\"\"", file=f)
+
+        print("export VARIKN_OPTIONS=\"--nfirst=60000 --dscale=0.0025\"", file=f)
+        count = 20000
+        print("export VARIKN_DEVSIZE={}".format(count), file=f)
+
+    with open("{}_b60kv_s_{}g_m.sh".format(lang,order), 'w') as f:
+        print("export TRAIN_NAME='{}_b60kv_s_{}g_m'".format(lang,order), file=f)
+        print("export TRAIN_DIR=$GROUP_DIR/p/sami/lmmodels/biglm/$TRAIN_NAME", file=f)
+        print(file=f)
+        print("export SOURCE_FILES=$GROUP_DIR/p/sami/lmdata/{}/biglm.txt".format(lang), file=f)
+        print(file=f)
+        print("export NGRAM_ORDER={}".format(order), file=f)
+        print("export PRUNE_THRESHOLD=5e-9", file=f)
+        print(file=f)
+        print("export LA_NGRAM_ORDER={}".format(order//3), file=f)
+        print("export LA_PRUNE_THRESHOLD=5e-8", file=f)
+
+        print("export MORPH_TRAIN_OPTIONS=\"\"", file=f)
+
+    with open("{}_b60kv_s_{}g_w.sh".format(lang,order), 'w') as f:
+        print("export TRAIN_NAME='{}_b60kv_s_{}g_w'".format(lang,order), file=f)
+        print("export TRAIN_DIR=$GROUP_DIR/p/sami/lmmodels/biglm/$TRAIN_NAME", file=f)
+        print(file=f)
+        print("export SOURCE_FILES=$GROUP_DIR/p/sami/lmdata/{}/biglm.txt".format(lang), file=f)
+        print(file=f)
+        print("export NGRAM_ORDER={}".format(order), file=f)
+        print("export PRUNE_THRESHOLD=5e-9", file=f)
+        print(file=f)
+        print("export LA_NGRAM_ORDER={}".format(order//3), file=f)
+        print("export LA_PRUNE_THRESHOLD=5e-8", file=f)
